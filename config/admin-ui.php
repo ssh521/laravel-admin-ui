f7b508922641967942a1080b7e8d3481d5c406d9
ssh521 <ssh521@users.noreply.github.com>
2026-06-21 08:17:16 +0900

    Add admin UI config

---
+++ config/admin-ui.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'prefix' => 'laravel-admin',
+
+    'theme' => [
+        'primary' => 'brand',
+        'neutral' => 'neutral',
+        'success' => 'success',
+        'warning' => 'warning',
+        'danger' => 'danger',
+    ],
+
+    'layout' => [
+        'radius' => 'xl',
+        'density' => 'comfortable',
+    ],
+
+    'components' => [
+        'button' => [
+            'default_variant' => 'solid',
+            'default_size' => 'md',
+        ],
+
+        'card' => [
+            'padding' => 'md',
+            'shadow' => true,
+        ],
+    ],
+];
