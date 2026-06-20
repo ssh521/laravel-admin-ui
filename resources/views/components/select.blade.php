c052b4c90db2f3809a6b89d154fd19d9c546af3b
ssh521 <ssh521@users.noreply.github.com>
2026-06-21 08:19:20 +0900

    Add select component

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,7 @@
+<select
+    {{ $attributes->merge([
+        'class' => 'h-10 w-full rounded-lg border border-neutral-300 bg-white px-3 text-sm text-neutral-800 focus:outline-none focus:ring-2 focus:ring-brand-400 disabled:opacity-50 disabled:cursor-not-allowed'
+    ]) }}
+>
+    {{ $slot }}
+</select>
